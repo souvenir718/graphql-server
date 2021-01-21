8ab570708a3e70d7efb6940c5b1fe266b2a09575
souvenir718 <mongkey0@naver.com>
2021-01-21 15:00:26 +0900

    Tutorial 9 Filteringm, Pagination, Sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,31 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links();
+async function feed(parent, args, context) {
+    // 1
+    const where = args.filter
+        ? {
+              OR: [{ description_contains: args.filter }, { url_contains: args.filter }],
+          }
+        : {};
+
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy,
+    });
+    const count = await context.prisma // 수정
+        // 2
+        .linksConnection({
+            // 수정
+            where, // 수정
+        }) // 수정
+        .aggregate() // 수정
+        .count(); // 수정
+    // 3
+    return {
+        // 수정
+        links, // 수정
+        count, // 수정
+    }; // 수정
 }
 
 module.exports = {
